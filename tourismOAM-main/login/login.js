ef7beb8099f4e72f3e87623134b7b347d49963d4
kiwa3 <mohma20041@gmail.com>
2025-12-04 20:26:38 +0200

    Remove redundant nested tourismOAM-main directory

--- tourismOAM-main/login/login.js
+++
@@ -1,8 +0,0 @@
-// Login Page JavaScript
-
-document.querySelector('.login-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // In a real application, this would handle authentication
-    window.location.href = '../dashboard/dashboard.html';
-});
-
